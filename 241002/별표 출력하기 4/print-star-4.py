4c5c8c44919f98294d6a04f21813ecdd33fba3fc
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-10-02 11:31:47 +0900

    Title: 별표 출력하기 4, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241002/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end=' ')
+    print()
+for i in range(2,n+1):
+    for j in range(i):
+        print("*",end=' ')
+    print()
