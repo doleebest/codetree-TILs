6a92a2e814888024124a4fcda788aeeffc6fa8c4
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-10-03 23:32:25 +0900

    Title: 별 그리기, Time: 213ms, Memory: 27MB, Status: Passed - CodeTree

--- 241003/별 그리기/star-drawing.py
+++ 241003/별 그리기/star-drawing.py
@@ -1,7 +1,7 @@
 import sys
 n = int(sys.stdin.readline())
 for i in range(n) :
-    for j in range(2-i) :
+    for j in range(n-i-1) :
         print(" ",end='')
     for k in range(2*i+1):
         print("*",end='')
