782c6929eafe4267c9c5ee33c8300c10584210de
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-10-04 16:21:57 +0900

    Title: 체크 다이아몬드, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241004/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+for i in range(n):
+    for j in range(n-1-i):
+        print(" ",end='')
+    for k in range(i+1) :
+        print("*",end=' ')
+    print()
+
+for i in range(n):
+    for j in range(i+1) :
+        print(" ",end='')
+    for k in range(n-1-i):
+        print("*",end=' ')
+    print()
