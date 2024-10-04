cc59decce7c6f20cd2ce8188cbdef57f2197beb8
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-10-04 16:22:51 +0900

    Title: 체크 다이아몬드, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241004/체크 다이아몬드/check-diamond.py
+++ 241004/체크 다이아몬드/check-diamond.py
@@ -2,14 +2,14 @@ import sys
 n = int(sys.stdin.readline())
 for i in range(n):
     for j in range(n-1-i):
-        print(" ",end='')
+        print("_",end='')
     for k in range(i+1) :
         print("*",end=' ')
     print()
 
 for i in range(n):
     for j in range(i+1) :
-        print(" ",end='')
+        print("_",end='')
     for k in range(n-1-i):
         print("*",end=' ')
     print()
