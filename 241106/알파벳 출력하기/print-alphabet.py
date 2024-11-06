7e353482fedcce56bd6712f1f94f274a7984371c
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-11-06 16:09:45 +0900

    Title: 알파벳 출력하기, Time: 41ms, Memory: 15MB, Status: Passed - CodeTree

--- 241106/알파벳 출력하기/print-alphabet.py
+++ 241106/알파벳 출력하기/print-alphabet.py
@@ -3,7 +3,7 @@ n = int(sys.stdin.readline())
 alphabet = 65
 for i in range(1,n+1) :
     for j in range(1,i+1) : 
-        if alphabet==90 :
+        if alphabet==91 :
             alphabet = 65
         print(chr(alphabet), end="")
         alphabet+=1
