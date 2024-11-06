613d66f21bc83605790af9997c4f394dc0eeb745
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-11-06 16:04:14 +0900

    Title: 정사각 알파벳 출력하기, Time: 38ms, Memory: 15MB, Status: Passed - CodeTree

--- 241106/정사각 알파벳 출력하기/print-alpabet-in-square.py
+++ 241106/정사각 알파벳 출력하기/print-alpabet-in-square.py
@@ -1,6 +1,7 @@
-import sys
-n = int(sys.stdin.readline())
-for i in range(n) :
-    for j in range(n) :
-        print(chr(i))
+n = int(input())
+alphabet = 65
+for i in range(1,n+1) :
+    for j in range(1,n+1) :
+        print(chr(alphabet), end="")
+        alphabet+=1
     print()
