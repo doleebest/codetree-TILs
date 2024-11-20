e3a98be10b1dd38df0a4ffce243087b5700ef796
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-11-20 11:55:55 +0900

    Title: 숫자 직사각형, Time: 59ms, Memory: 17MB, Status: Passed - CodeTree

--- 241120/숫자 직사각형/rectangle-of-numbers.py
+++ 241120/숫자 직사각형/rectangle-of-numbers.py
@@ -1,5 +1,12 @@
 n,m = map(int, input().split())
-arr_2d = [list(map(int, input().split())) for _ in range(n)]
+arr_2d = [ [0 for _ in range(m)] for _ in range(n)]
+num = 1
+for i in range(n) :
+    for j in range(m) :
+        arr_2d[i][j] = num
+        num+=1
+
+
 for i in range(n) :
     for j in range(m) :
         print(arr_2d[i][j], end=" ")
